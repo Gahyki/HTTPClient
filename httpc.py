0ecb59b2b86a95ba7489b8681ca7730a1c708652
phong1233 <le.phong0499@gmail.com>
2022-02-11 17:25:25 -0500

    add redirect

--- httpc.py
+++ httpc.py
@@ -32,40 +32,68 @@ def get_file_data(file):
     return boundary, content_length, data
 
 
-def send_request(host, port, encoded_text, verbose):
-    # Setting up socket for TCP connection
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        # Attempt to connect at port 80
-        conn.connect((host, port))
-        request = str.encode(encoded_text)
-        # Send request
-        conn.send(request)
-        # Get http response and print according to verbose settings
-        response = conn.recv(10000)
-        response = response.decode("utf-8")
-        if verbose:
-            print(response)
-        else:
-            print(response[response.index("\r\n\r\n") + 4 :])
+def output(response, verbose, file=None):
+    result = response if verbose else response[response.index("\r\n\r\n") + 4 :]
+    if file:
+        try:
+            f = open(file, "w")
+            f.write(result)
+        except:
+            print("There was an error outputting into file.")
+            print(result)
+        finally:
+            f.close()
+    else:
+        print(result)
 
-    finally:
-        # Close TCP connection
-        conn.close()
+
+def get_response_code(response):
+    status = int(response.split()[1])
+    return status
+
+def get_new_location(response):
+    res = response.split()
+    idx = res.index('location:')
+    location = res[idx+1]
+    return location
+
+def send_request(host, port, encoded_text, verbose, filename, path, query, method, redirect):
+    for _ in range(10):
+        # Setting up socket for TCP connection
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            # Attempt to connect at port 80
+            conn.connect((host, port))
+            request = str.encode(f"{method} {path}{query} HTTP/1.1\r\n" + encoded_text)
+            # Send request
+            conn.send(request)
+            # Get http response and print according to verbose settings
+            response = conn.recv(100000)
+            response = response.decode("utf-8")
+            status_code = get_response_code(response)
+            if redirect and status_code == 302:
+                path = get_new_location(response)
+                print(f"Redirecting to {path}...")
+            else:
+                output(response, verbose, filename)
+                break
+        finally:
+            # Close TCP connection
+            conn.close()
 
 
 # -------------------- Request Methods ------------------------
-def run_get(verbose, header, url):
+def run_get(verbose, header, url, filename, redirect):
     # Deconstructing url to get required data
     host, path, query = deconstruct_url(url)
     # Assemble header(s) and text
     header_str = "\r\n".join([": ".join(h.split(":")) for h in header]) + "\r\n"
-    string_to_send = f"GET {path}{query} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n{header_str}\r\n\r\n"
+    string_to_send = f"Host: {host}\r\nConnection: close\r\n{header_str}\r\n\r\n"
     # Send request
-    send_request(host, 80, string_to_send, verbose)
+    send_request(host, 80, string_to_send, verbose, filename, path, query, "GET", redirect)
 
 
-def run_post(verbose, header, data, file, url):
+def run_post(verbose, header, data, file, url, filename, redirect):
     # Deconstructing url
     host, path, query = deconstruct_url(url)
     # Assembling request data
@@ -78,9 +106,9 @@ def run_post(verbose, header, data, file, url):
         boundary, content_length, data = get_file_data(file)
         content_type = f"multipart/form-data; boundary={boundary}"
     header_str = "\r\n".join([": ".join(h.split(":")) for h in header]) + "\r\n"
-    string_to_send = f"POST {path}{query} HTTP/1.1\r\nHost: {host}\r\n{header_str}Connection: close\r\nContent-Length: {content_length}\r\nContent-Type: {content_type}\r\n\r\n{data}\r\n"
+    string_to_send = f"Host: {host}\r\n{header_str}Connection: close\r\nContent-Length: {content_length}\r\nContent-Type: {content_type}\r\n\r\n{data}\r\n"
     # Send request
-    send_request(host, 80, string_to_send, verbose)
+    send_request(host, 80, string_to_send, verbose, filename, path, query, "POST", redirect)
 
 
 # -------------------- Main Method ------------------------
@@ -155,11 +183,23 @@ Use "httpc help [command]" for more information about a command.
         default=[],
         required=False,
     )
+    parser.add_argument(
+        "-r",
+        help="Specify if we want to redirect or not",
+        action="store_true",
+        default=False,
+        required=False,
+    )
     parser.add_argument("url")
-
+    parser.add_argument(
+        "-o",
+        help="Specify the filename where the HTTP client will write the body of the response",
+        default=None,
+        required=False,
+    )
     if cmdargs[1] == "get":
         args = parser.parse_args()
-        run_get(args.v, args.h, args.url)
+        run_get(args.v, args.h, args.url, args.o, args.r)
 
     elif cmdargs[1] == "post":
         action = parser.add_mutually_exclusive_group(required=False)
@@ -174,7 +214,7 @@ Use "httpc help [command]" for more information about a command.
             required=False,
         )
         args = parser.parse_args()
-        run_post(args.v, args.h, args.d, args.f, args.url)
+        run_post(args.v, args.h, args.d, args.f, args.url, args.o, args.r)
 
     else:
         print("Missing arguments. Run [help] for usage")
